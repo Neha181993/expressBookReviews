4f94b53b455b32ae09b595b0073f178f311e2cab
Neha181993 <140276999+Neha181993@users.noreply.github.com>
2024-10-21 06:17:05 -0700

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+const axios = require('axios')
 
 const app = express();
 
@@ -30,7 +31,7 @@ app.use("/customer/auth/*", function auth(req,res,next){
     }
 });
 
-const PORT =5000;
+const PORT =5030;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
